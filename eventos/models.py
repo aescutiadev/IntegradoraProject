05d8c0ed38959fc519e490d7c3787273a2955c1a
noa-index <abyss.floyd@gmail.com>
2019-08-19 16:02:05 -0500

    version7

--- eventos/models.py
+++ eventos/models.py
@@ -5,14 +5,14 @@ class Eventos(models.Model):
 	"""Model definition for Eventos."""
 	# TODO: Define fields here
 	name = models.CharField(max_length=50,verbose_name="Nombre")
-	estado = models.ForeignKey('Estados', on_delete=models.CASCADE, blank=True, null=True, verbose_name="Estado")
+	estado = models.ForeignKey('Estados', on_delete=models.CASCADE, verbose_name="Estado")
 	city = models.CharField(max_length=50,verbose_name="Ciudad")
 	descripcion = models.TextField(verbose_name="Descripción")
 	date = models.DateField(auto_now=False, auto_now_add=False, verbose_name="Fecha del Evento")
 	hora = models.TimeField(auto_now=False, auto_now_add=False, verbose_name="Hora del Evento")
-	km = models.CharField( max_length=12, verbose_name="Kilómetros de recorrido",blank=True,null=True)
-	time = models.TimeField(auto_now=False, auto_now_add=False, verbose_name="Tiempo de recorrido")
-	punto_inicio = models.CharField(max_length=50,verbose_name="Punto de inicio",blank=True,null=True)
+	km = models.CharField( max_length=20, verbose_name="Kilómetros de recorrido",blank=True,null=True)
+	time = models.CharField(max_length=70,verbose_name="Tiempo de recorrido")
+	punto_inicio = models.CharField(max_length=200,verbose_name="Punto de inicio")
 	costo = models.FloatField(verbose_name="Costo")
 	imagen = models.ImageField(upload_to='evento', height_field=None, width_field=None, max_length=None)
 	created = models.DateTimeField(auto_now_add=True,verbose_name="Creación") # Fecha y tiempo
